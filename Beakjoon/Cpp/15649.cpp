8c117782480f2b05ec7cf4cfea0d0d528cf6dbea
GloryDay <alwaysgloryday@gmail.com>
2022-01-27 04:37:35 +0900

    Upload baekjoon problem solution

---
+++ Beakjoon/Cpp/15649.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+bool visited[9];
+vector<int> v;
+
+void print() {
+    for (int& i: v) {
+        cout << i << ' ';
+    }
+    cout << '\n';
+}
+
+void f(int size, int n, int m) {
+    if (visited[m]) return;
+
+    visited[m] = true;
+    v.push_back(m);
+    for (int i = 1; i <= size; i++) {
+        if (!visited[i]) {
+            f(size, n - 1, i);
+        }
+    }
+
+    if (n == 1) {
+        print();
+    }
+
+    visited[m] = false;
+    v.pop_back();
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    
+    for (int i = 1; i <= n; i++) {
+        f(n, m, i);
+    }
+}
