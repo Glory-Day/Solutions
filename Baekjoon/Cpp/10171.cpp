b3a1fb83be7e0fd31b81c556482d36bef5b6a022
Glory-Day <alwaysgloryday@gmail.com>
2022-10-03 23:42:42 +0900

    Delete folder Baekjoon, Codeforces, Leetcode

--- Baekjoon/Cpp/10171.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-int main() {
-    cout << "\\    /\\\n )  ( ')\n(  /  )\n \\(__)|";
-}
--- Baekjoon/Cpp/11654.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-int main() {
-    char c;
-    cin >> c;
-    cout << (int)c;
-}
