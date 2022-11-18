95862754b2d1eb096d2439bd9c93888bf15ab01c
Glory-Day <alwaysgloryday@gmail.com>
2022-11-18 19:14:25 +0900

    Upload programmers problem solution

---
+++ Programmers/Level-01/Cpp/옹알이_2.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+int solution(vector<string> babbling) {
+    int answer = 0;
+
+    string cache[4] = { "aya","ye","woo","ma" };
+
+    for (int i = 0; i < babbling.size(); i++) {
+        for (int j = 0; j < 4; j++) {
+            size_t pos = 0;
+            while ((pos = babbling[i].find(cache[j], pos)) != string::npos) {
+                babbling[i].replace(pos, cache[j].length(), "*");
+                pos += 2;
+            }
+        }
+
+        answer++;
+
+        for (int j = 0; j < babbling[i].size(); j++) {
+            if (babbling[i][j] != '*') {
+                answer--;
+                break;
+            }
+        }
+    }
+
+    return answer;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<string> babbling;
+    for (int i = 0; i < n; i++) {
+        string input;
+        cin >> input;
+        babbling.push_back(input);
+    }
+
+    int answer = solution(babbling);
+
+    cout << answer;
+}
