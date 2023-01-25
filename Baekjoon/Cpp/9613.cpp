aa2a6e94ca5e7967ecc72b191912eb3600e0bf4a
Glory-Day <alwaysgloryday@gmail.com>
2023-01-25 19:54:43 +0900

    Upload baekjoon problem solution

---
+++ Baekjoon/Cpp/9613.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int gcd(int a, int b) {
+    if (b == 0) {
+        return a;
+    }
+
+    return gcd(b, a % b);
+}
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        vector<int> arr;
+        for (int i = 0; i < n; i++) {
+            int input;
+            cin >> input;
+            arr.push_back(input);
+        }
+
+        vector<int> bits = { 1,1 };
+        for (int i = 2; i < n; i++) {
+            bits.push_back(0);
+        }
+
+        long long answer = 0;
+        do {
+            int tmp[2];
+            for (int i = 0, j = 0; i < n; i++) {
+                if (bits[i]) {
+                    tmp[j++] = arr[i];
+                }
+            }
+
+            answer += gcd(tmp[0], tmp[1]);
+        }
+        while (prev_permutation(bits.begin(), bits.end()));
+
+        cout << answer << '\n';
+    }
+}
