59ee5bc6cb34616b91a5114dfe917c4a2eed1e57
Glory Day <alwaysgloryday@gmail.com>
2024-11-06 16:31:31 +0900

    Upload programmers problem solution

---
+++ Programmers/Level-00/Cpp/정수_부분.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(double flo) {
+    int answer = (int)flo;
+    
+    return answer;
+}
+
+int main()
+{
+    double flo;
+    cin >> flo;
+
+    int answer = solution(flo);
+
+    cout << answer;
+
+    return 0;
+}
