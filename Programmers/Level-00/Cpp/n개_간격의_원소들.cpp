5c5f6f3ac5bf9d0647a3a4dc1d3ae60dbf1000a8
Glory Day <alwaysgloryday@gmail.com>
2024-11-06 16:29:44 +0900

    Upload programmers problem solution

---
+++ Programmers/Level-00/Cpp/n개_간격의_원소들.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> num_list, int n) {
+    vector<int> answer;
+    
+    for (int i = 0; i < (int)num_list.size(); i += n)
+    {
+        answer.push_back(num_list[i]);
+    }
+    
+    return answer;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<int> num_list(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> num_list[i];
+    }
+
+    cin >> n;
+
+    vector<int> answer = solution(num_list, n);
+    
+    for (int i = 0; i < (int)answer.size(); i++)
+    {
+        cout << answer[i] << ' ';
+    }
+
+    return 0;
+}
