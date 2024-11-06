fbc0d4356c8fd796187fe221e65502474f01dc6c
Glory Day <alwaysgloryday@gmail.com>
2024-11-06 16:31:07 +0900

    Upload programmers problem solution

---
+++ Programmers/Level-00/Cpp/배열의_원소만큼_추가하기.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr) {
+    vector<int> answer;
+    
+    for (int i = 0; i < (int)arr.size(); i++)
+    {
+        for (int j = 0; j < arr[i]; j++)
+        {
+            answer.push_back(arr[i]);
+        }
+    }
+    
+    return answer;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    vector<int> answer = solution(arr);
+    
+    for (int i = 0; i < (int)answer.size(); i++)
+    {
+        cout << answer[i] << ' ';
+    }
+
+    return 0;
+}
