f9dabd6dd705b8644e24b5d537849af6cc7b1d19
Glory Day <alwaysgloryday@gmail.com>
2024-11-11 14:56:41 +0900

    Upload progremmers problem solution

---
+++ Programmers/Level-01/Cpp/가장_가까운_같은_글자.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(string s) {
+    vector<int> answer;
+
+    vector<int> cache(26, -1);
+    for (int i = 0; i < s.size(); i++) {
+        int tmp = s[i] - 'a';
+        if (cache[tmp] != -1) {
+            answer.push_back(i - cache[tmp]);
+        }
+        else {
+            answer.push_back(cache[tmp]);
+        }
+        
+        cache[tmp] = i;
+    }
+
+    return answer;
+}
+
+int main()
+{
+    string s;
+    cin >> s;
+
+    vector<int> answer = solution(s);
+
+    for (int i = 0; i < (int)answer.size(); i++)
+    {
+        cout << answer[i] << ' ';
+    }
+
+    return 0;
+}
