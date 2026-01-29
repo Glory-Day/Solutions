1678407e50e42a311442e86900be568ada39fb7d
Glory Day <alwaysgloryday@gmail.com>
2026-01-30 04:49:05 +0900

    Upload baekjoon problem solution

--- Baekjoon/Cpp/6064.cpp
+++ Baekjoon/Cpp/6064.cpp
@@ -1,42 +1,46 @@
 #include <iostream>
-#include <vector>
+
+#define FAST_IO ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
 using namespace std;
 
-int gcd(int a, int b) {
-    if (b == 0) {
-        return a;
-    }
-
-    return gcd(b, a % b);
+inline int gcd(const int lhs, const int rhs)
+{
+    return rhs == 0 ? lhs : gcd(rhs, lhs % rhs);
 }
 
-int lcm(int a, int b) {
-    return (a * b) / gcd(a, b);
+inline int lcm(const int lhs, const int rhs)
+{
+    return (lhs * rhs) / gcd(lhs, rhs);
 }
 
-int main() {
+int main()
+{
+    FAST_IO;
+
     int t;
     cin >> t;
 
-    while (t--) {
+    while (t--)
+    {
         int n, m, x, y;
         cin >> n >> m >> x >> y;
 
-        int mx = lcm(n, m);
-        while (x <= mx) {
-            if ((x - 1) % m + 1 == y || x > mx) {
+        int cache = lcm(n, m);
+        while (x <= cache)
+        {
+            if ((x - 1) % m + 1 == y || x > cache)
+            {
                 break;
             }
 
             x += n;
         }
 
-        if (x > mx) {
-            cout << -1 << '\n';
-        }
-        else {
-            cout << x << '\n';
-        }
+        int answer = (x > cache ? -1 : x);
+
+        cout << answer << '\n';
     }
+
+    return 0;
 }
