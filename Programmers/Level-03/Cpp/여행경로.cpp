6b7a4385485650e22b2c1ad89c51aad3c99713fb
Glory Day <alwaysgloryday@gmail.com>
2026-02-03 07:51:48 +0900

    Upload programmers problems solution

--- Programmers/Level-03/Cpp/여행경로.cpp
+++ Programmers/Level-03/Cpp/여행경로.cpp
@@ -1,58 +1,73 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <queue>
-#include <unordered_map>
 #include <algorithm>
+#include <functional>
 
 using namespace std;
 
-vector<bool> visited;
-
-bool dfs(string nd, vector<vector<string>>& tickets, vector<string>& answer, int lv) {
-    answer.push_back(nd);
-
-    if (lv == tickets.size()) return true;
-
-    for (int i = 0; i < tickets.size(); i++) {
-        if (tickets[i][0] == nd && !visited[i]) {
-            visited[i] = true;
-
-            if (dfs(tickets[i][1], tickets, answer, lv + 1)) return true;
-            else visited[i] = false;
-        }
-    }
-
-    answer.pop_back();
-
-    return false;
-}
-
-vector<string> solution(vector<vector<string>> tickets) {
+vector<string> solution(vector<vector<string>> tickets)
+{
     vector<string> answer;
 
+    int size = (int)tickets.size();
+
     sort(tickets.begin(), tickets.end());
+    
+    vector<bool> visited(size, false);
+    vector<string> path;
+    
+    function<bool(string, int)> dfs = [&](string current, int depth)
+    {
+        path.push_back(current);
+    
+        if (depth == size)
+        {
+            return true;
+        }
 
-    visited = vector<bool>(tickets.size(), false);
-    dfs("ICN", tickets, answer, 0);
+        for (int i = 0; i < size; i++)
+        {
+            if (visited[i] == false && tickets[i][0] == current)
+            {
+                visited[i] = true;
+                if (dfs(tickets[i][1], depth + 1))
+                {
+                    return true;
+                }
+                visited[i] = false;
+            }
+        }
+        
+        path.pop_back();
 
+        return false;
+    };
+
+    dfs("ICN", 0);
+
+    answer = path;
+    
     return answer;
 }
 
-int main() {
+int main()
+{
     int n;
     cin >> n;
 
-    vector<vector<string>> tickets;
-    for (int i = 0; i < n; i++) {
-        string a, b;
-        cin >> a >> b;
-        tickets.push_back({ a,b });
+    vector<vector<string>> tickets(n, vector<string>(2));
+    for (int i = 0; i < n; i++)
+    {
+        cin >> tickets[i][0] >> tickets[i][1];
     }
 
     vector<string> answer = solution(tickets);
 
-    for (auto& i: answer) {
+    for (auto& i: answer)
+    {
         cout << i << ' ';
     }
+
+    return 0;
 }
