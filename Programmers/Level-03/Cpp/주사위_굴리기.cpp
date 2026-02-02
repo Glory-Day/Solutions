67b3f7e088cda9c959e69adb2f354634c3e21ac9
Glory Day <alwaysgloryday@gmail.com>
2026-02-03 07:52:14 +0900

    Upload programmers problems solution

--- Programmers/Level-03/Cpp/주사위_굴리기.cpp
+++ Programmers/Level-03/Cpp/주사위_굴리기.cpp
@@ -1,95 +1,133 @@
+#include <iostream>
 #include <vector>
 #include <algorithm>
+#include <functional>
 
 using namespace std;
 
-void dfs(vector<vector<int>>& v, vector<int>& caches, const int n, const int depth, const int lv){
-    if((int)caches.size() == depth){
-        v.push_back(caches);
-        
-        return;
-    }
-
-    for(int i = lv; i < n; i++){
-        caches.push_back(i + 1);
-
-        dfs(v, caches, n, depth, i + 1);
-        
-        caches.pop_back();
-    }
-}
-
-void dfs(vector<int>& out, vector<vector<int>> dice, vector<int>& indexes, int sum, const int lv){
-    if(lv == (int)indexes.size()){
-        out.push_back(sum);
-
-        return;
-    }
-
-    for(int i = 0; i < (int)dice[indexes[lv] - 1].size(); i++){
-        sum += dice[indexes[lv] - 1][i];
-
-        dfs(out, dice, indexes, sum, lv + 1);
-        
-        sum -= dice[indexes[lv] - 1][i];
-    }
-}
-
-vector<int> solution(vector<vector<int>> dice) {
+vector<int> solution(vector<vector<int>> dice)
+{
     vector<int> answer;
     
-    
-    int n = (int)dice.size();
-    vector<vector<int>> v;
-    vector<int> caches;
+    int size = (int)dice.size();
+    vector<vector<int>> combinations;
+    vector<int> visited;
 
-    dfs(v, caches, n, n / 2, 0);
-
-    int b = 0, e = (int)v.size() - 1;
-    int mx = 0;
-
-    while(b < e)
+    function<void(int)> get_combinations = [&](int start)
     {
-        int sum1 = 0, sum2 = 0;
-        vector<int> l, r;
-        dfs(l, dice, v[b], sum1, 0);
-        dfs(r, dice, v[e], sum2, 0);
+        if ((int)visited.size() == size / 2)
+        {
+            combinations.push_back(visited);
 
-        sort(l.begin(), l.end());
-        sort(r.begin(), r.end());
+            return;
+        }
 
-        int count1= 0, count2 = 0;
-        for (auto i : l){
-            int j = lower_bound(r.begin(), r.end(), i) - r.begin();
-            if (j >= 0)
+        for (int i = start; i < size; i++)
+        {
+            visited.push_back(i);
+            get_combinations(i + 1);
+            visited.pop_back();
+        }
+    };
+
+    get_combinations(0);
+
+    int cache = 0;
+    for (int i = 0; i < (int)combinations.size() / 2; i++)
+    {
+        vector<int> dices[2];
+        vector<int> sums[2];
+
+        function<void(int, int, int)> get_sums = [&](int index, int sum, int depth)
+        {
+            if (depth == (int)dices[index].size())
             {
-                count1 += j;
+                sums[index].push_back(sum);
+                return;
+            }
+
+            int i = dices[index][depth];
+            for (int j = 0; j < 6; j++)
+            {
+                get_sums(index, sum + dice[i][j], depth + 1);
+            }
+        };
+
+        vector<int> used(size, 0);
+
+        for (int combination : combinations[i])
+        {
+            dices[0].push_back(combination);
+
+            used[combination] = 1;
+        }
+
+        for (int j = 0; j < size; j++)
+        {
+            if (used[j] == false)
+            {
+                dices[1].push_back(j);
             }
         }
-        for (auto i : r){
-            int j = lower_bound(l.begin(), l.end(), i) - l.begin();
-            if (j >= 0)
-            {
-                count2 += j;
-            }
+
+        get_sums(0, 0, 0);
+        get_sums(1, 0, 0);
+
+        long long caches[2] = { 0,0 };
+
+        sort(sums[1].begin(), sums[1].end());
+        for (int j : sums[0])
+        {
+            caches[0] += lower_bound(sums[1].begin(), sums[1].end(), j) - sums[1].begin();
         }
 
-        if (count1 > count2 && count1 > mx){
-            answer = v[b];
-            mx = count1;
-        }
-        else if (count2 > count1 && count2 > mx){
-            answer = v[e];
-            mx = count2;
+        sort(sums[0].begin(), sums[0].end());
+        for (int j : sums[1])
+        {
+            caches[1] += lower_bound(sums[0].begin(), sums[0].end(), j) - sums[0].begin();
         }
 
-        l.clear();
-        r.clear();
-        
-        b++;
-        e--;
+        if (caches[0] > cache)
+        {
+            cache = (int)caches[0];
+            answer = dices[0];
+        }
 
+        if (caches[1] > cache)
+        {
+            cache = (int)caches[1];
+            answer = dices[1];
+        }
+    }
+
+    for (int& index : answer)
+    {
+        index++;
     }
 
     return answer;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<vector<int>> dices(n, vector<int>(6));
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < 6; j++)
+        {
+            cin >> dices[i][j];
+        }
+    }
+
+    vector<int> answer = solution(dices);
+
+    for (int i = 0; i < (int)answer.size(); i++)
+    {
+        cout << answer[i] << ' ';
+    }
+
+    return 0;
 }
